eb02b8965dd5aeaf8a8896fc4e6297ccad4cbcc8
Laura Constantin <lconstantin@tremend.ro>
2015-10-14 10:42:57 +0300

    dashbord

--- Projects/Dashboard/scripts/main.js
+++ Projects/Dashboard/scripts/main.js
@@ -18,28 +18,40 @@ dataContainer.appendChild(mainCheckbox);
 for (var i = 0; i < items.length; i++) {
     var item = items[i];
 
-    var itemContainer = document.createElement("li");
-    var itemDataContainer = document.createElement("ul");
+    // var itemContainer = document.createElement("li");
+    // var itemDataContainer = document.createElement("ul");
 
-    var idContainer = document.createElement("li");
+    // var idContainer = document.createElement("li");
     /**
      * innerHtml - only for html content
      * innerText - not supported by firefox
      * textContent - purrrfect
      */
-    idContainer.textContent = item.id;
-    itemDataContainer.appendChild(idContainer);
 
-    var checkboxContainer = document.createElement("li");
+    var item = items[i].name;
+    var nameList = document.createElement("ul");
+    var nameContainer = document.createElement("li");
+
+    nameContainer.textContent = items[i].name;
+    nameList.appendChild(nameContainer);
+
+    
+    dataContainer.appendChild(nameList);
+
+    var checkboxContainer = document.createElement("span");
     var checkbox = document.createElement("input");
     checkbox.setAttribute("type", "checkbox");
     checkbox.setAttribute("class", "checkbox_item");
     checkboxContainer.appendChild(checkbox);
-    itemDataContainer.appendChild(checkboxContainer);
+    nameContainer.appendChild(checkboxContainer);
 
-    itemContainer.appendChild(itemDataContainer);
-    itemsContainer.appendChild(itemContainer);
-}
+    // checkboxContainer.textContent = items[i].id;
+    // nameContainer.appendChild(checkboxContainer);
+    // // itemDataContainer.appendChild(checkboxContainer);
+
+    // // itemContainer.appendChild(itemDataContainer);
+    // // itemsContainer.appendChild(itemContainer);
+ }
 
 
 dataContainer.appendChild(itemsContainer);
